935e15956685acb950d10d79e5dd9ebbbeebb697
mister-benoit <benoit.sepe@gmail.com>
2015-10-21 21:23:05 +0200

    Implémentation de toutes les structures

--- Colonie/src/fr/benoitsepe/colonie/structures/interieur/Refectoire.java
+++ Colonie/src/fr/benoitsepe/colonie/structures/interieur/Refectoire.java
@@ -1,5 +1,20 @@
 package fr.benoitsepe.colonie.structures.interieur;
 
-public class Refectoire {
+import fr.benoitsepe.colonie.main.IStructure;
+import fr.benoitsepe.colonie.main.Ressources;
+import fr.benoitsepe.colonie.main.Structure;
+
+public class Refectoire extends Structure implements IStructure {
+
+	public Refectoire() {
+		super("Réfectoire");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void utiliser(Ressources res) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- Colonie/src/fr/benoitsepe/colonie/structures/interieur/Sas.java
+++ Colonie/src/fr/benoitsepe/colonie/structures/interieur/Sas.java
@@ -1,5 +1,20 @@
 package fr.benoitsepe.colonie.structures.interieur;
 
-public class Sas {
+import fr.benoitsepe.colonie.main.IStructure;
+import fr.benoitsepe.colonie.main.Ressources;
+import fr.benoitsepe.colonie.main.Structure;
 
+public class Sas extends Structure implements IStructure{
+
+	public Sas() {
+		super("S.A.S.");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void utiliser(Ressources res) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
