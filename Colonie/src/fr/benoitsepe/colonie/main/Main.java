f43da41f047720307286f5e92f80a3522ba6c2b2
mister-benoit <benoit.sepe@gmail.com>
2015-10-21 16:53:23 +0200

    ajout d'annotation

--- Colonie/src/fr/benoitsepe/colonie/main/Main.java
+++ Colonie/src/fr/benoitsepe/colonie/main/Main.java
@@ -8,8 +8,12 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		
-		Gestion moteur = new Gestion();
-		moteur.creerStruct(TypeStructures.EOLIENNE);
+		// Je t'ai mis un exemple de création d'une éolienne, lance le programme pour voir
+		
+		Gestion moteur = new Gestion(); // Init moteur
+		
+		
+		moteur.creerStruct(TypeStructures.EOLIENNE); // Tu peux en créer plusieurs si tu veux, j'ai pas encore implémenté les autres structures
 	}
 
 }
