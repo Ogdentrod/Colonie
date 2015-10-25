3564d9248a19cc5166ee55b282c9009f4f148d29
Pop115 <kienan42@gmail.com>
2015-10-25 18:19:45 +0100

    HELLO
    Ajout de quelques sprites pour test (dans ressources)
    Ajout du Cage

--- Colonie/src/fr/kienanbachwa/colonie/affichage/Fenetre.java
+++ Colonie/src/fr/kienanbachwa/colonie/affichage/Fenetre.java
@@ -1,11 +1,6 @@
 package fr.kienanbachwa.colonie.affichage;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -17,7 +12,14 @@ public class Fenetre extends BasicGame
 	public GameContainer gc;
 	String texte = "Surprise!";
 	Image background;
-	private AppGameContainer app;
+	Image playerImage;
+	private MapLoader mapLoader;
+	
+	private float y;
+	private float delta = 5;
+	private float x;
+	
+	private boolean[] direction = new boolean[4];
 	
 	public Fenetre(String gamename)
 	{
@@ -28,15 +30,24 @@ public class Fenetre extends BasicGame
 	public void init(GameContainer gc) throws SlickException {
 		this.gc=gc;
 		background = new Image("ressources/cage.jpg");
-		if(gc instanceof AppGameContainer){
-			
-		}
+		playerImage = new Image("ressources/cage.png");
+		mapLoader = new MapLoader();
+		System.out.println(mapLoader.getMap().getTileId(1,1,0));
+		
 	}
 
 	public void keyReleased(int key, char c){
 		if (Input.KEY_ESCAPE==key){
 			gc.exit();
 		}
+		
+		 switch (key) {
+		 
+	        case Input.KEY_UP:    this.direction[0] = false; break;
+	        case Input.KEY_LEFT:  this.direction[1] = false; break;
+	        case Input.KEY_DOWN:  this.direction[2] = false; break;
+	        case Input.KEY_RIGHT: this.direction[3] = false; break;
+	    }
 	}
 	
 	public void keyPressed(int key, char c){
@@ -46,19 +57,43 @@ public class Fenetre extends BasicGame
 			}else{
 				texte=texte+c;
 			}
+			
+			 switch (key) {
+			 
+		        case Input.KEY_UP:    this.direction[0] = true; break;
+		        case Input.KEY_LEFT:  this.direction[1] = true; break;
+		        case Input.KEY_DOWN:  this.direction[2] = true; break;
+		        case Input.KEY_RIGHT: this.direction[3] = true; break;
+		    }
 	}
+	
 	
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {
-		
+		if(this.direction[0]){
+			this.y -= .1f * delta;
+		}
+		if(this.direction[1]){
+			this.x -= .1f * delta;
+		}
+		if(this.direction[2]){
+			this.y += .1f * delta;
+		}
+		if(this.direction[3]){
+			this.x += .1f * delta;
+		}
 	}
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
+		/*
 		background.draw(0,0,640,480);
 		g.setColor(Color.red);
 		g.scale(1.5f,1.5f);
 		g.drawString(texte, 180, 80);
+		*/
+		mapLoader.getMap().render(0, 0);
+		g.drawImage(playerImage, x, y);
 	}
 }
--- Colonie/src/fr/kienanbachwa/colonie/affichage/Map.java
+++
@@ -1,9 +0,0 @@
-package fr.kienanbachwa.colonie.affichage;
-
-public class Map {
-
-	/*
-	 * http://slick.ninjacave.com/javadoc/ -> org.newdawn.slick.tiled
-	 * http://www.shionn.org/slick2d-lesson-2-map-display
-	 */
-}
