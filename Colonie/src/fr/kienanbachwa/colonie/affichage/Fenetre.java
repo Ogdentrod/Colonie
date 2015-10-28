bb628e6eac1f067be863559c1104c183c66ec0c4
Pop115 <kienan42@gmail.com>
2015-10-28 20:23:46 +0100

    Ajout zoom+limites map, enlevé personnage

--- Colonie/src/fr/kienanbachwa/colonie/affichage/Fenetre.java
+++ Colonie/src/fr/kienanbachwa/colonie/affichage/Fenetre.java
@@ -16,10 +16,14 @@ public class Fenetre extends BasicGame
 	private MapLoader mapLoader;
 	
 	private float y;
-	private float delta = 5;
 	private float x;
+	private float delta = 10;
+	
+	private int mapSizeX;
+	private int mapSizeY;
 	
 	private boolean[] direction = new boolean[4];
+	private float zoom = 1;
 	
 	public Fenetre(String gamename)
 	{
@@ -29,11 +33,14 @@ public class Fenetre extends BasicGame
 	@Override
 	public void init(GameContainer gc) throws SlickException {
 		this.gc=gc;
-		background = new Image("ressources/cage.jpg");
-		playerImage = new Image("ressources/cage.png");
 		mapLoader = new MapLoader();
 		System.out.println(mapLoader.getMap().getTileId(1,1,0));
+		x=gc.getWidth()/2;
+		y=gc.getHeight()/2;
 		
+	    mapSizeY = mapLoader.getMap().getHeight() * mapLoader.getMap().getTileHeight();
+	    mapSizeX = mapLoader.getMap().getWidth() * mapLoader.getMap().getTileWidth();
+
 	}
 
 	public void keyReleased(int key, char c){
@@ -64,22 +71,27 @@ public class Fenetre extends BasicGame
 		        case Input.KEY_LEFT:  this.direction[1] = true; break;
 		        case Input.KEY_DOWN:  this.direction[2] = true; break;
 		        case Input.KEY_RIGHT: this.direction[3] = true; break;
+		        
+		        case Input.KEY_SUBTRACT: this.zoom-=0.2f; 
+		        	System.out.println("zoom:"+zoom); break;
+		        case Input.KEY_ADD: this.zoom+=0.2f; 
+		        	System.out.println("zoom:"+zoom); break;
 		    }
 	}
 	
 	
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {
-		if(this.direction[0]){
+		if(this.direction[0] && (y - gc.getHeight()/2 > 0)){
 			this.y -= .1f * delta;
 		}
-		if(this.direction[1]){
+		if(this.direction[1] && (x - gc.getWidth()/2 >0)){
 			this.x -= .1f * delta;
 		}
-		if(this.direction[2]){
+		if(this.direction[2] && (y+gc.getHeight()/2 < mapSizeY)){
 			this.y += .1f * delta;
 		}
-		if(this.direction[3]){
+		if(this.direction[3] && (x+gc.getWidth()/2 < mapSizeX )){
 			this.x += .1f * delta;
 		}
 	}
@@ -93,7 +105,9 @@ public class Fenetre extends BasicGame
 		g.scale(1.5f,1.5f);
 		g.drawString(texte, 180, 80);
 		*/
-		mapLoader.getMap().render(0, 0);
-		g.drawImage(playerImage, x, y);
+		
+		g.scale(zoom, zoom);
+	    g.translate(gc.getWidth() / 2 - (int)this.x,  gc.getHeight() / 2 - (int)this.y); 
+	    mapLoader.getMap().render(0, 0);
 	}
 }
