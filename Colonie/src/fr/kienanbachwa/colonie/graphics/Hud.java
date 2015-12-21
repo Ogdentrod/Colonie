3f05daa1d3f8d8b911e220591f8c31c916d4b28e
Pop115 <kienan42@gmail.com>
2015-12-21 13:02:34 +0100

    Ajout HUD

---
+++ Colonie/src/fr/kienanbachwa/colonie/graphics/Hud.java
@@ -0,0 +1,21 @@
+package fr.kienanbachwa.colonie.graphics;
+
+public class Hud {
+	
+	public Hud(){
+		
+	}
+	
+	public void init(){
+		
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void render(){
+		
+	}
+
+}
