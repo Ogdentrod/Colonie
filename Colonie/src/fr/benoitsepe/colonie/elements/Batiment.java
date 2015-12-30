b4261ede652874ad9c65903ff57142738d6b0b09
mister-benoit <benoit.sepe@gmail.com>
2015-12-30 18:48:02 +0100

    réduction temps construction

--- Colonie/src/fr/benoitsepe/colonie/elements/Batiment.java
+++ Colonie/src/fr/benoitsepe/colonie/elements/Batiment.java
@@ -3,7 +3,7 @@ package fr.benoitsepe.colonie.elements;
 public class Batiment extends Element{
 
 	public Batiment(String name, int x, int y, Etat etat) {
-		super(name, 1000, x, y, etat);
+		super(name, 100, x, y, etat);
 		// TODO Auto-generated constructor stub
 	}
 
