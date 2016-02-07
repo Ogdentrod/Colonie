7a2cc5f389638262b6a9419de95aafb70c423b0e
Benoît Sepe <benoit.sepe@gmail.com>
2016-02-07 17:45:44 +0100

    Correctif bug sol/mur : intégration de porte en tant que batiment

--- Colonie/src/fr/benoitsepe/colonie/structures/Porte.java
+++ Colonie/src/fr/benoitsepe/colonie/structures/Porte.java
@@ -1,14 +1,14 @@
 package fr.benoitsepe.colonie.structures;
 
-public class Porte extends Structure{
+public class Porte extends Batiment{
 
 	public Porte(int x, int y) {
-		super("Porte", 2000, x, y, Etat.QUEUED);
+		super("Porte", x, y, Etat.QUEUED);
 		// TODO Auto-generated constructor stub
 	}
 	
 	public Porte(int x, int y, Etat etat) {
-		super("Porte", 2000, x, y, etat);
+		super("Porte", x, y, etat);
 		// TODO Auto-generated constructor stub
 	}
 
